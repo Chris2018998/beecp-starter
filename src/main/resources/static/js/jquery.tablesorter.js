2e0f66edb97612b78a900949a22a30bd46d571d8
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2021-02-24 19:52:29 +0800

    Add files via upload

--- src/main/resources/static/js/jquery.tablesorter.js
+++ src/main/resources/static/js/jquery.tablesorter.js
@@ -197,8 +197,7 @@
                 }
 
                 return list;
-            };
-
+            }
             function detectParserForColumn(table, rows, rowIndex, cellIndex) {
                 var l = parsers.length,
                     node = false,
@@ -283,15 +282,12 @@
                     cache.normalized.push(cols);
                     cols = null;
                 }
-                ;
-
                 if (table.config.debug) {
                     benchmark("Building cache for " + totalRows + " rows:", cacheTime);
                 }
 
                 return cache;
-            };
-
+            }
             function getElementText(config, node) {
 
                 var text = "";
@@ -372,8 +368,7 @@
                     $(table).trigger("sortEnd");
                 }, 0);
 
-            };
-
+            }
             function buildHeaders(table) {
 
                 if (table.config.debug) {
@@ -412,8 +407,7 @@
 
                 return $tableHeaders;
 
-            };
-
+            }
             // from:
             // http://www.javascripttoolbox.com/lib/table/examples.php
             // http://www.javascripttoolbox.com/temp/table_cellindex.html
@@ -433,7 +427,7 @@
                         var rowIndex = c.parentNode.rowIndex;
                         var cellId = rowIndex + "-" + c.cellIndex;
                         var rowSpan = c.rowSpan || 1;
-                        var colSpan = c.colSpan || 1
+                        var colSpan = c.colSpan || 1;
                         var firstAvailCol;
                         if (typeof (matrix[rowIndex]) == "undefined") {
                             matrix[rowIndex] = [];
@@ -478,13 +472,11 @@
                     }
                 }
                 return arr;
-            };
-
+            }
             function checkHeaderMetadata(cell) {
                 if (($.metadata) && ($(cell).metadata().sorter === false)) {
                     return true;
                 }
-                ;
                 return false;
             }
 
@@ -492,7 +484,6 @@
                 if ((table.config.headers[i]) && (table.config.headers[i].sorter === false)) {
                     return true;
                 }
-                ;
                 return false;
             }
 
@@ -518,8 +509,7 @@
                         return widgets[i];
                     }
                 }
-            };
-
+            }
             function formatSortingOrder(v) {
                 if (typeof (v) != "Number") {
                     return (v.toLowerCase() == "desc") ? 1 : 0;
@@ -564,7 +554,6 @@
                     });
                     $(table).prepend(colgroup);
                 }
-                ;
             }
 
             function updateHeaderSortCount(table, sortList) {
@@ -634,8 +623,7 @@
                 }
 
                 return cache;
-            };
-
+            }
             function makeSortFunction(type, direction, index) {
                 var a = "a[" + index + "]",
                     b = "b[" + index + "]";
@@ -648,45 +636,36 @@
                 } else if (type == 'numeric' && direction == 'desc') {
                     return "(" + a + " === null && " + b + " === null) ? 0 :(" + a + " === null ? Number.POSITIVE_INFINITY : (" + b + " === null ? Number.NEGATIVE_INFINITY : " + b + " - " + a + "));";
                 }
-            };
-
+            }
             function makeSortText(i) {
                 return "((a[" + i + "] < b[" + i + "]) ? -1 : ((a[" + i + "] > b[" + i + "]) ? 1 : 0));";
-            };
-
+            }
             function makeSortTextDesc(i) {
                 return "((b[" + i + "] < a[" + i + "]) ? -1 : ((b[" + i + "] > a[" + i + "]) ? 1 : 0));";
-            };
-
+            }
             function makeSortNumeric(i) {
                 return "a[" + i + "]-b[" + i + "];";
-            };
-
+            }
             function makeSortNumericDesc(i) {
                 return "b[" + i + "]-a[" + i + "];";
-            };
-
+            }
             function sortText(a, b) {
                 if (table.config.sortLocaleCompare) return a.localeCompare(b);
                 return ((a < b) ? -1 : ((a > b) ? 1 : 0));
-            };
-
+            }
             function sortTextDesc(a, b) {
                 if (table.config.sortLocaleCompare) return b.localeCompare(a);
                 return ((b < a) ? -1 : ((b > a) ? 1 : 0));
-            };
-
+            }
             function sortNumeric(a, b) {
                 return a - b;
-            };
-
+            }
             function sortNumericDesc(a, b) {
                 return b - a;
-            };
-
+            }
             function getCachedSortType(parsers, i) {
                 return parsers[i].type;
-            }; /* public methods */
+            } /* public methods */
             this.construct = function (settings) {
                 return this.each(function () {
                     // if no thead or tbody quit.
@@ -766,7 +745,6 @@
                                         config.sortList.push([i, this.order]);
                                     }
                                 }
-                                ;
                                 setTimeout(function () {
                                     // set css for headers
                                     setHeadersCss($this[0], $headers, config.sortList, sortCSS);
@@ -845,7 +823,6 @@
                 if (a) {
                     parsers.push(parser);
                 }
-                ;
             };
             this.addWidget = function (widget) {
                 widgets.push(widget);
@@ -874,7 +851,7 @@
                     table.tBodies[0].innerHTML = "";
                 }
             };
-        }
+        };
     });
 
     // extend plugin scope
--- src/test/java/cn/beecp/boot/test/TestSocket.java
+++ src/test/java/cn/beecp/boot/test/TestSocket.java
@@ -17,24 +17,25 @@ package cn.beecp.boot.test;
 
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketAddress;
+
 public class TestSocket {
-    public static void main(String[]args){
-        testSocket("localhost",3306);
+    public static void main(String[] args) {
+        testSocket("localhost", 3306);
     }
-    private static void testSocket(String host,int port){
+
+    private static void testSocket(String host, int port) {
         Socket socket = new Socket();
-        long time1=System.currentTimeMillis();
+        long time1 = System.currentTimeMillis();
         try {
             socket.connect(new InetSocketAddress(host, port));
             socket.close();
-        }catch(Exception e) {
-        }finally {
-            long time=System.currentTimeMillis()-time1;
-            if(socket.isConnected()){
-                System.out.println("Connect success,Time："+time+"ms");
-            }else{
-                System.out.println("Connect failed,Time："+time+"ms");
+        } catch (Exception e) {
+        } finally {
+            long time = System.currentTimeMillis() - time1;
+            if (socket.isConnected()) {
+                System.out.println("Connect success,Time：" + time + "ms");
+            } else {
+                System.out.println("Connect failed,Time：" + time + "ms");
             }
         }
     }
