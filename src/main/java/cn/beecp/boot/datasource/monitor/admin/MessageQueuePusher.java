c345a43e1a9814273a8e1c9468285cc5aa15d869
Chris2018998 <32663325+Chris2018998@users.noreply.github.com>
2022-04-05 19:37:03 +0800

    Update MessageQueuePusher.java

--- src/main/java/cn/beecp/boot/datasource/monitor/admin/MessageQueuePusher.java
+++ src/main/java/cn/beecp/boot/datasource/monitor/admin/MessageQueuePusher.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package cn.beecp.boot.datasource.admin;
+package cn.beecp.boot.datasource.monitor.admin;
 
 import redis.clients.jedis.Jedis;
 
